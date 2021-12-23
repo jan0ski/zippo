987e19008811ee61eea433dfe84312d8c51b18ba
jan0ski <mccormickt9@gmail.com>
2021-12-22 21:45:03 -0500

    Clean up package

--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
+
 	zippo "github.com/jan0ski/zippo/pkg"
+	log "github.com/sirupsen/logrus"
 )
 
+var (
+	listenAddr   string
+	listenPort   int
+	sshUser      string
+	sshPubkey    string
+	k8sVersion   string
+	templatePath string
+)
+
+func init() {
+	flag.StringVar(&listenAddr, "address", "0.0.0.0", "listen address of the server")
+	flag.IntVar(&listenPort, "port", 8080, "key to add to `authorized_hosts` for ssh access")
+	flag.StringVar(&sshUser, "ssh-user", "core", "initial user available via ssh")
+	flag.StringVar(&sshPubkey, "ssh-pubkey", "", "key to add to `authorized_hosts` for ssh access")
+	flag.StringVar(&k8sVersion, "k8s", "v1.20.14", "Kubernetes version to install")
+	flag.StringVar(&templatePath, "template", "/etc/zippo/template.yaml", "path to Butane template")
+	flag.Parse()
+}
+
+// Define your own args to pass into the template
+// Don't include `Hostname`, its populated from the host header
+type args struct {
+	SSHUser       string
+	SSHPubkey     string
+	CNIVersion    string
+	CRICtlVersion string
+	K8sVersion    string
+}
+
 func main() {
-	s := &zippo.Server{
-		Config: &zippo.ServerConfig{
-			ButaneTemplate: "./config.yaml",
-		},
+	args := args{
+		SSHUser:    sshUser,
+		SSHPubkey:  sshPubkey,
+		K8sVersion: k8sVersion,
 	}
-	s.Run()
+
+	// overwrite config file with rendered variables
+	template, err := zippo.Render(templatePath, args)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	ioutil.WriteFile(templatePath, template.Bytes(), 0600)
+
+	zippo.NewServer(listenAddr, listenPort, templatePath, args).Run()
 }
--- pkg/zippo.go
+++ pkg/zippo.go
@@ -2,79 +2,47 @@ package zippo
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
-	"net/http"
 	"text/template"
 
 	"github.com/coreos/butane/config"
 	"github.com/coreos/butane/config/common"
-	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
-type args struct {
-	Hostname string
-}
-
-type Server struct {
-	Config *ServerConfig
-}
-
-type ServerConfig struct {
-	Address        string
-	Port           int
-	ButaneTemplate string
-}
-
-func (s *Server) Run() {
-	router := gin.Default()
-	router.GET("/ignition", s.serveButaneTranslator)
-	router.Run()
-}
-
-func (s *Server) serveButaneTranslator(c *gin.Context) {
-	// Set hostname from host header
-	hostname := c.Request.Host
-	if hostname == "" {
-		c.JSON(http.StatusInternalServerError, nil)
-		return
-	}
-	log.Print(hostname)
-
-	// Translate human-readable Butane config to Ignition
-	ignitionConfig, err := createIgnitionConfig(hostname, s.Config.ButaneTemplate)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, nil)
-		return
-	}
-	c.JSON(http.StatusOK, string(ignitionConfig))
-}
-
-func createIgnitionConfig(hostname, butaneFile string) ([]byte, error) {
+// Render renders a Butane config template file with variables from `args`
+func Render(templatePath string, args interface{}) (*bytes.Buffer, error) {
 	// Parse config template
-	file, err := ioutil.ReadFile(butaneFile)
+	file, err := ioutil.ReadFile(templatePath)
 	if err != nil {
 		return nil, err
 	}
 
-	butaneTemplate, err := template.New("butaneConfig").Parse(string(file))
+	template, err := template.New("butaneConfig").Parse(string(file))
 	if err != nil {
 		return nil, err
 	}
 
-	// Render butane config template with given hostname
-	butaneConfig := &bytes.Buffer{}
-	err = butaneTemplate.Execute(butaneConfig, args{Hostname: hostname})
+	// Render config template with given arguments
+	config := &bytes.Buffer{}
+	err = template.Execute(config, args)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+// CreateIgnitionConfig creates an ignition config from a rendered butane template with a given hostname
+func CreateIgnitionConfig(butaneTemplate, hostname string) ([]byte, error) {
+	butaneConfig, err := Render(butaneTemplate, hostname)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(butaneConfig.String())
 
 	ignitionConfig, r, err := config.TranslateBytes(butaneConfig.Bytes(), common.TranslateBytesOptions{Pretty: true})
 	if err != nil {
-		return nil, errors.Wrapf(err, "Error translating config: %s", r.String())
+		return nil, errors.Wrapf(err, "error translating config: %s", r.String())
 	}
 
 	return ignitionConfig, nil
